66c0650909bd60f823fbe18ab5cb8bc6e0fc1f63
jackbackrack <jrb@pobox.com>
2017-01-06 01:54:37 -0800

    split out ao

--- Makefile.mk
+++ Makefile.mk
@@ -1,17 +1,6 @@
-stanza_geom_all: ${GEN}/ao.pkg ${GEN}/ao-gfx.pkg ${GEN}/ao-eval.pkg ${GEN}/geom.pkg ${GEN}/geom-gfx.pkg 
-
-LIBS += -lao
+stanza_geom_all: ${GEN}/geom.pkg ${GEN}/geom-gfx.pkg 
 
 ALL_PKG_DEPS += stanza_geom_all
-
-${GEN}/ao.pkg: stanza-geom/ao.stanza ${GEN}/utils.pkg ${GEN}/geom.pkg ${GEN}/font.pkg ${GEN}/glu.pkg
-	stanza $< $(STZ_FLAGS)
-
-${GEN}/ao-gfx.pkg: stanza-geom/ao-gfx.stanza ${GEN}/gl.pkg ${GEN}/geom.pkg ${GEN}/geom-gfx.pkg ${GEN}/ao.pkg
-	stanza $< $(STZ_FLAGS)
-
-${GEN}/ao-eval.pkg: stanza-geom/ao-eval.stanza ${GEN}/utils.pkg ${GEN}/eval.pkg ${GEN}/geom.pkg ${GEN}/glu.pkg ${GEN}/ao.pkg ${GEN}/font.pkg
-	stanza $< $(STZ_FLAGS)
 
 ${GEN}/geom.pkg: stanza-geom/geom.stanza ${GEN}/utils.pkg 
 	stanza $< $(STZ_FLAGS)
