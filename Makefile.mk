00d9815f755936a1ff772aac601c8de33a4c9dc3
jackbackrack <jrb@pobox.com>
2017-02-07 16:53:35 -0800

    new repl support

--- Makefile.mk
+++ Makefile.mk
@@ -5,6 +5,12 @@ ALL_PKG_DEPS += stanza_geom_all
 ${GEN}/geom.pkg: stanza-geom/geom.stanza ${GEN}/utils.pkg 
 	stanza $< $(STZ_FLAGS)
 
+${GEN}/eval-geom.stanza: ${GEN}/gen-repl ${GEN}/geom.pkg
+	${GEN}/gen-repl geom
+
+${GEN}/eval-geom.pkg: ${GEN}/eval-geom.stanza ${BASE_EVAL_PKGS} ${GEN}/geom.pkg
+	stanza $< $(STZ_FLAGS)
+
 ${GEN}/geom-gfx.pkg: stanza-geom/geom-gfx.stanza ${GEN}/geom.pkg ${GEN}/gfx.pkg ${GEN}/font.pkg
 	stanza $< $(STZ_FLAGS)
 
